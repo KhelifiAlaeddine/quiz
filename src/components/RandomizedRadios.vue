058bc43b6865fa6baa7ec73e0720326055c7c779
Marwen Khamassi <marwen.khamassi-prestataire@laposte.fr>
2024-04-14 21:59:58 +0200

    us-4 : change the verification method + amelioration affichage

--- src/components/RandomizedRadios.vue
+++ src/components/RandomizedRadios.vue
@@ -20,22 +20,45 @@ export default {
       selectedOptions,
       shuffledOptions
     };
+  },
+  methods: {
+    verifyResponse(option) {
+      document.querySelectorAll('.answer').forEach((el) => el.classList.remove('answer-good', 'answer-bad'));
+
+      if(option.correct)  {
+        if(document.querySelector('#response-'+option.id) ){
+          document.querySelector('#response-'+option.id).classList.add("answer-good");
+        }        
+        
+      } else {
+        if(document.querySelector('#response-'+option.id) ){
+          document.querySelector('#response-'+option.id).classList.add("answer-bad");
+        } 
+      }
+      this.$emit('verify-response', option.value);
+   }
   }
 };
 </script>
 <template>
   <div v-for="(option, index) in shuffledOptions" :key="index">
-    <input type="radio" :id="'option' + index" :value="option.name" v-model="selectedOptions">
-    <label :for="'option' + index" :class="`answer-${option.correct}`">{{ option.name }}</label>
+    <fieldset class="answer" :id="'response-' + option.id">
+      <input type="radio" :id="'option' + index"  :value="option.name" v-model="selectedOptions" @change="verifyResponse(option)">
+      <label :for="'option' + index">{{ option.name }}</label>
+    </fieldset>
+    
   </div>
 </template>
-<style scoped>
-input:checked {
-  & + .answer-true{
-    color: green;
+<style lang="scss">
+.answer {
+  input,label {
+    cursor: pointer;
   }
-  & + .answer-false {
-    color: red;
+  &-good {
+    background-color: lightgreen;
+  }
+  &-bad {
+    background-color: lightcoral;
   }
 }
 </style>
