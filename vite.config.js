4a76d046a6b349b58407008f94f6940deed00ebc
Marwen Khamassi <marwen.khamassi-prestataire@laposte.fr>
2024-06-04 13:23:21 +0200

    fixing tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom"
